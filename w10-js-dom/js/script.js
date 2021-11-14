e12d3c30e8891571d0cef2c76360164c8f605e73
Lea <lr2896@nyu.edu>
2021-11-14 07:20:07 -0500

    First push!

---
+++ w10-js-dom/js/script.js
@@ -0,0 +1,54 @@
+
+let x = 0;
+let red = 0;
+
+function change() {
+  //x += 10;
+  red += 10;
+
+  let b = document.getElementById('box');
+  b.innerHTML = "WOW!"
+  //b.style.width = "200px";
+  //b.style.height = "50px";
+  b.style.left = x + "px";
+  b.style.backgroundColor = "rgb(" + red + " , 255, 0)";
+}
+
+function addDiv() {
+  // create a HTML element
+  let elt = document.createElement("div");
+  // update styles
+  elt.style.backgroundColor = "cyan";
+  elt.style.width = "50px";
+  elt.style.height = "40px";
+  elt.style.margin = "20px";
+
+  // attach to the document!
+  //let body = document.body;
+  //body.appendChild(elt);
+  // document.body.appendChild(elt);
+
+  let box = document.getElementById('box');
+  box.appendChild(elt);
+  //document.getElementById('box').appendChild(elt);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
